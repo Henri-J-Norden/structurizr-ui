023d998c6c5727b72f4417e7b7a8ae4bbd12b930
Simon Brown <help@structurizr.com>
2025-11-01 15:19:25 +0000

    Fixes an issue exporting diagrams when element names/descriptions/etc include emojis.

--- src/js/structurizr-diagram.js
+++ src/js/structurizr-diagram.js
@@ -5028,7 +5028,7 @@ structurizr.ui.Diagram = function(id, diagramIsEditable, constructionCompleteCal
             }
         };
 
-        img.src = 'data:image/svg+xml;base64,' + btoa(svgMarkup);
+        img.src = 'data:image/svg+xml;base64,' + structurizr.util.btoa(svgMarkup);
     }
 
 
